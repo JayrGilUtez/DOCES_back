ab8461b975ccecb7e6495394906c9b5ae0e4cdf5
JayrGil <20223tn052@utez.edu.mx>
2024-10-17 12:29:03 -0500

    first commit

---
+++ src/main/java/mx/edu/utez/doces_back/DocesBackApplication.java
@@ -0,0 +1,14 @@
+package mx.edu.utez.doces_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocesBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocesBackApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/doces_back/DocesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.doces_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocesBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
