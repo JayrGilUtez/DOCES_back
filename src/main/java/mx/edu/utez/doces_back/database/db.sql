b6eac665f9f2df2a433e5d8a119b4975d97873b2
Carlos-gmz <carlos.mtzg2004@gmail.com>
2024-10-21 16:57:24 -0600

    feat: :zap: Se agregó el script de la base de datos y una clase de utilidades

---
+++ src/main/java/mx/edu/utez/doces_back/database/db.sql
@@ -0,0 +1,26 @@
+create database doces;
+
+use doces;
+
+create table Role(
+    id int primary key auto_increment,
+    role varchar(30) not null UNIQUE
+);
+
+create table Person(
+    id int primary key auto_increment,
+    name varchar(50) not null,
+    last_name varchar(50) not null,
+    id_student int not null UNIQUE
+); 
+
+create table User(
+    id int primary key auto_increment,
+    username varchar(50) not null UNIQUE,
+    email varchar(50) not null UNIQUE,
+    password varchar(256) not null,
+    id_role int not null,
+    id_person int not null,
+    foreign key(id_role) references Role(id),
+    foreign key(id_person) references Person(id)
+);
