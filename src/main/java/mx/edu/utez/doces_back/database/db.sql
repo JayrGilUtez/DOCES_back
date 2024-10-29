99202e7f08b4043540f909acf5d1a735350046d4
Carlos-Mtzg <carlos.mtzg2004@gmail.com>
2024-10-29 12:42:51 -0600

    feat: Se modificó la estructura de la base de datos
    Se cambiaron los nombres de algunos cambios de la base de datos para las tablas relacionadas con el usuario

--- src/main/java/mx/edu/utez/doces_back/database/db.sql
+++ src/main/java/mx/edu/utez/doces_back/database/db.sql
@@ -1,26 +1,27 @@
-create database doces;
+create database doces_database;
 
-use doces;
+use doces_database;
 
 create table Role(
-    id int primary key auto_increment,
-    role varchar(30) not null UNIQUE
+    `id` int primary key auto_increment,
+    `name` varchar(30) not null UNIQUE
 );
 
 create table Person(
-    id int primary key auto_increment,
-    name varchar(50) not null,
-    last_name varchar(50) not null,
-    id_student int not null UNIQUE
-); 
+    `id` int primary key auto_increment,
+    `name` varchar(50) not null,
+    `last_name` varchar(50) not null,
+    -- Esta es la matrícula ej.20223TN066
+    `student_id` int not null UNIQUE
+);
 
 create table User(
-    id int primary key auto_increment,
-    username varchar(50) not null UNIQUE,
-    email varchar(50) not null UNIQUE,
-    password varchar(256) not null,
-    id_role int not null,
-    id_person int not null,
+    `id` int primary key auto_increment,
+    `username` varchar(50) not null UNIQUE,
+    `email` varchar(50) not null UNIQUE,
+    `password` varchar(256) not null,
+    `id_role` int not null,
+    `id_person` int not null,
     foreign key(id_role) references Role(id),
     foreign key(id_person) references Person(id)
 );
