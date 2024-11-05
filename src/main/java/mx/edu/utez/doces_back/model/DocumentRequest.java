ffef58b75db8d1fca1f2658e3edf4c115225f2ac
JayrGil <122942269+JayrGilUtez@users.noreply.github.com>
2024-11-05 14:30:15 -0600

    [ADD] Realationships

---
+++ src/main/java/mx/edu/utez/doces_back/model/DocumentRequest.java
@@ -0,0 +1,34 @@
+package mx.edu.utez.doces_back.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "document_requests")
+public class DocumentRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer user_id;
+    private Integer admin_id;
+    private String status;
+    private Integer id_formato_de_certificacion_de_estudios;
+    private Integer id_recibo_de_pago;
+    private Integer id_constancia_de_no_adeudo;
+    private Integer id_carta_libereacion_de_estadias;
+
+    @ManyToMany(mappedBy = "documentRequests")
+    private Set<UserModel> userModels = new HashSet<>();
+
+    @OneToMany(mappedBy = "documentRequest")
+    private Set<File> files = new HashSet<>();
+
+}
