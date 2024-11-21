c9e107ad4a914bda36b10b796291e4f96c303bce
JayrGil <122942269+JayrGilUtez@users.noreply.github.com>
2024-11-21 12:20:31 -0600

    Stop tracking application.properties

---
+++ src/main/java/mx/edu/utez/doces_back/controller/DocumentRequestController.java
@@ -0,0 +1,15 @@
+package mx.edu.utez.doces_back.controller;
+
+
+
+import mx.edu.utez.doces_back.model.DocumentRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/documentRequest")
+public class DocumentRequestController {
+
+
+}
