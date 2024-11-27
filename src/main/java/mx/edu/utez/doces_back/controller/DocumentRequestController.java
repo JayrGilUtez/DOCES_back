9f8e013798596c10c5eca9cb145448bb8ea5e851
JayrGil <122942269+JayrGilUtez@users.noreply.github.com>
2024-11-27 14:53:53 -0600

    feat(controller): Document request
    Get docuemt request by userId feature added
    
    Author: Jayr_Gil

--- src/main/java/mx/edu/utez/doces_back/controller/DocumentRequestController.java
+++ src/main/java/mx/edu/utez/doces_back/controller/DocumentRequestController.java
@@ -2,7 +2,11 @@ package mx.edu.utez.doces_back.controller;
 
 
 
+import mx.edu.utez.doces_back.config.ApiResponse;
 import mx.edu.utez.doces_back.model.DocumentRequest;
+import mx.edu.utez.doces_back.service.DocumentRequestService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/documentRequest")
 public class DocumentRequestController {
+    private final DocumentRequestService documentRequestService;
+
+    public DocumentRequestController(DocumentRequestService documentRequestService) {
+        this.documentRequestService = documentRequestService;
+    }
+
+    @PostMapping("/{userId}")
+    public ResponseEntity<ApiResponse> createDocumentRequest(@PathVariable Integer userId) {
+        return documentRequestService.createDocumentRequest(userId);
+    }
 
 
 }
