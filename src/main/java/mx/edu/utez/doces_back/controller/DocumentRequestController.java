eeaf94129218a46cdb25edc2fcbf6ba7460d0ddb
JayrGil <122942269+JayrGilUtez@users.noreply.github.com>
2024-11-30 17:53:14 -0600

    feat(controller): DocumentRequest
    CrossOrigin anotation added
    
    Author: Jayr_Gil

--- src/main/java/mx/edu/utez/doces_back/controller/DocumentRequestController.java
+++ src/main/java/mx/edu/utez/doces_back/controller/DocumentRequestController.java
@@ -8,7 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/documentRequest")
+@CrossOrigin(origins = "*")
+@RequestMapping("api/documentRequest")
 public class DocumentRequestController {
     private final DocumentRequestService documentRequestService;
 
